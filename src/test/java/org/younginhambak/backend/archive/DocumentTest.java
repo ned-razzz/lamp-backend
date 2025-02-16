8718e3527bfd7eeb7c7f0f8a83f697d00b025def
nedrazzz <smg9972@gmail.com>
2025-02-16 21:04:10 +0900

    test: Document, Photo domain function test

---
+++ src/test/java/org/younginhambak/backend/archive/DocumentTest.java
@@ -0,0 +1,133 @@
+package org.younginhambak.backend.archive;
+
+import org.junit.jupiter.api.Test;
+import org.younginhambak.backend.file.FileExtension;
+import org.younginhambak.backend.member.Member;
+import org.younginhambak.backend.tag.Tag;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class DocumentTest {
+
+  @Test
+  public void 자료저장() throws Exception {
+    //given
+    List<DocumentFile> documentFiles = createDocumentFiles();
+    ArrayList<DocumentTag> documentTags = createDocumentTags();
+
+    //when
+    Document document = createDocument(documentFiles, documentTags);
+
+    //then
+    assertThat(document.getMember().getName()).isEqualTo("member1");
+    assertThat(document.getFiles()).containsAll(documentFiles);
+    assertThat(document.getDocumentTags()).containsAll(documentTags);
+  }
+
+  @Test
+  public void 자료수정() throws Exception {
+    //given
+    List<DocumentFile> documentFiles = createDocumentFiles();
+    ArrayList<DocumentTag> documentTags = createDocumentTags();
+    Document document = createDocument(documentFiles, documentTags);
+
+    //when
+    Member memberU = Member.create("memberu", "memberu@gmail.com", "nicknameu");
+
+    DocumentFile fileU = DocumentFile.create("fileu", "urlu", FileExtension.TXT);
+    ArrayList<DocumentFile> filesU = new ArrayList<>();
+    filesU.add(documentFiles.get(1));
+    filesU.add(fileU);
+
+    Tag tagU = Tag.create("tagU");
+    Tag tagU2 = Tag.create("tagU2");
+    DocumentTag documentTagU = DocumentTag.create(tagU);
+    DocumentTag documentTagU2 = DocumentTag.create(tagU2);
+    ArrayList<DocumentTag> documentTagsU = new ArrayList<>();
+    documentTagsU.add(documentTags.get(0));
+    documentTagsU.add(documentTagU);
+    documentTagsU.add(documentTagU2);
+
+    document.update("titleu", "descriptionu", "authoru",
+            memberU,
+            filesU,
+            documentTagsU
+            );
+
+    //then
+    assertThat(document.getMember().getNickname()).isEqualTo(memberU.getNickname());
+
+    assertThat(document.getFiles()).hasSize(2);
+    assertThat(document.getFiles()).containsAll(filesU);
+    assertThat(document.getFiles()).doesNotContain(documentFiles.get(0));
+
+    assertThat(document.getDocumentTags()).hasSize(3);
+    assertThat(document.getDocumentTags()).containsAll(documentTagsU);
+    assertThat(document.getDocumentTags()).doesNotContain(documentTags.get(1));
+
+    assertThat(documentFiles.get(0).getDocument()).isNull();
+    assertThat(documentTags.get(1).getDocument()).isNull();
+  }
+
+  @Test
+  public void 자료삭제() throws Exception {
+    //given
+    List<DocumentFile> documentFiles = createDocumentFiles();
+    ArrayList<DocumentTag> documentTags = createDocumentTags();
+    Document document = createDocument(documentFiles, documentTags);
+
+    //when
+    document.delete();
+
+    //then
+    assertThat(document.getStatus()).isEqualTo(DocumentStatus.DELETED);
+    assertThat(document.getFiles().size()).isEqualTo(0);
+    assertThat(document.getDocumentTags().size()).isEqualTo(0);
+    documentFiles.forEach(documentFile -> {
+      assertThat(documentFile.getDocument()).isNull();
+    });
+    documentTags.forEach(documentTag -> {
+      assertThat(documentTag.getDocument()).isNull();
+      assertThat(documentTag.getTag()).isNull();
+    });
+
+
+  }
+
+  public Document createDocument(List<DocumentFile> files, List<DocumentTag> documentTags) {
+    Member member1 = Member.create("member1", "member@gmail.com", "nickname1");
+
+    return Document.create(
+            "title1",
+            "description1",
+            "author1",
+            member1,
+            files,
+            documentTags
+    );
+  }
+
+  private static ArrayList<DocumentTag> createDocumentTags() {
+    Tag tag1 = Tag.create("tag1");
+    Tag tag2 = Tag.create("tag2");
+    DocumentTag documentTag1 = DocumentTag.create(tag1);
+    DocumentTag documentTag2 = DocumentTag.create(tag2);
+    ArrayList<DocumentTag> documentTags = new ArrayList<>();
+    documentTags.add(documentTag1);
+    documentTags.add(documentTag2);
+    return documentTags;
+  }
+
+  public List<DocumentFile> createDocumentFiles() {
+    DocumentFile file1 = DocumentFile.create("file1", "url1", FileExtension.PDF);
+    DocumentFile file2 = DocumentFile.create("file2", "url2", FileExtension.DOC);
+    ArrayList<DocumentFile> files = new ArrayList<>();
+    files.add(file1);
+    files.add(file2);
+    return files;
+  }
+}
